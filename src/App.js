174507d271fd03cc64eb3372bf5e367b58745f69
Puay Ni Yi <puayny@gmail.com>
2021-03-29 02:26:16 +0800

    Add placeholders for main classes of Trello-like clone
    Add skeleton code for Board, Subboard, and TaskCard, the main
    components of a Trello-like clone.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
+
+/** Each TaskCard represents a single task */
+class TaskCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Task card</div>
+    )
+  }
+}
+
+/**
+ * One Subboard is used for each stage of task progression. 
+ * Each Subboard consists of multiple TaskCards.
+*/
+class Subboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <TaskCard />
+    )
+  }
+}
+
+/**
+ * Board represents the overall task board
+ * Each Board consists of multiple Subboards
+ */
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Subboard />
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <Board />
     </div>
   );
 }
