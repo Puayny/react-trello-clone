b8b43f8de793218b72ef4e62d84cd551e109dd9b
Puay Ni Yi <puayny@gmail.com>
2021-03-30 02:56:11 +0800

    Add drag and drop functionality, and simulate having a DB

--- src/App.js
+++ src/App.js
@@ -1,47 +1,59 @@
 import React from 'react';
 import './App.css';
+import * as DbUtils from './utils/db-utils.js'
 
-/**
- * Create unique task card ID
- * For this exercise, assume that max 1 task card 
- * created per user per millisecond
- * @param {string} userId
- * @param {Object} db
- * @returns task card ID
- */
-function createTaskCardId(userId) {
-  return "USER" + userId + "TIME" + Date.now();
-}
 
 // For this exercise, assume there's only one user
 const USER_ID = 1;
+let DB = {};
 
 /** Each TaskCard represents a single task */
 class TaskCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      "id": createTaskCardId(USER_ID)
+      taskCardHtmlId: this.props.taskCardHtmlId,
+      taskCardId: this.props.taskCardId,
+      taskCardIndex: this.props.taskCardIndex,
+      db: this.props.db,
     }
   }
 
-  // When element is dragged
-  elementDrag(event) {
+  // When task card is dragged. Track id of dragged task card 
+  handleTaskCardDragStart(event) {
     event.dataTransfer.setData('text', event.target.id);
   }
 
+  // Save changes made to task card inputs to db
+  handleInputChange(db, event, cardPortion) {
+    const newText = event.target.value;
+    DbUtils.saveTaskCardChanges(db, this.state.taskCardId, newText, cardPortion)
+  }
+
   render() {
+    const taskCardDetails = DbUtils.retrieveTaskCardDetails(this.state.db, this.state.taskCardId);
     return (
       <section
-        className="task-card" id={this.state.id}
+        className="task-card"
+        id={this.props.taskCardHtmlId}
+        data-task-card-id={this.props.taskCardId}
         draggable={true}
-        onDragStart={(event) => this.elementDrag(event)}
+        onDragStart={(event) => this.handleTaskCardDragStart(event)}
+        onDragEnd={(event) => this.props.onDragEnd(event, this.state.taskCardIndex, this.state.taskCardHtmlId)}
       >
         <header className="task-card-header">
-          <input type="text" defaultValue="Task header"></input>
+          <input type="text"
+            defaultValue={taskCardDetails["header"]}
+            placeholder="Task header"
+            onChange={(event) => this.handleInputChange(this.state.db, event, "header")}
+          ></input>
         </header>
         <div className="task-card-content">
-          <textarea rows="5" defaultValue="Task card content"></textarea>
+          <textarea rows="5"
+            defaultValue={taskCardDetails["content"]}
+            placeholder="Task content"
+            onChange={(event) => this.handleInputChange(this.state.db, event, "content")}
+          ></textarea>
         </div>
       </section >
     )
@@ -50,34 +62,67 @@ class TaskCard extends React.Component {
 
 /**
  * One Subboard is used for each stage of task progression. 
- * Each Subboard consists of multiple TaskCards.
+ * Each Subboard consists of multiple TaskCards, which are saved in its state.
+ * The state is then used to render the TaskCards, so if TaskCards are 
+ * shifted (moved, created, deleted), just change the state and the rendered HTML
+ * will change accordingly
 */
 class Subboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       taskCardIds: [],
+      db: this.props.db,
     }
   }
 
-  // When another element is dragged over a subboard
-  allowElementDrop(event) {
+  // Prevent default drag over behavior when task card is dragged over a subboard
+  handleTaskCardDragOver(event) {
     event.preventDefault();
   }
 
-  // When another element is dragged and dropped onto a subboard
-  elementDrop(event) {
+  // Move task card to new subboard when it is dragged and dropped over it
+  handleTaskCardDrop(event) {
+    // Ignore non-task card drops
+    // There's definitely a better way
+    const taskCardHtmlId = event.dataTransfer.getData('text');
+    const taskCard = document.getElementById(taskCardHtmlId);
+    if (taskCard === null || !taskCard.classList.contains("task-card")) {
+      return;
+    }
+
     event.preventDefault();
-    const elementId = event.dataTransfer.getData('text');
-    const subboard = event.target.closest(".subboard");
-    subboard.appendChild(document.getElementById(elementId));
+    // Only move task card when it is dragged to a subboard
+    if (event.target.closest(".subboard") !== null) {
+      const taskCardId = taskCard.dataset.taskCardId;
+      this.setState({ taskCardIds: this.state.taskCardIds.concat([taskCardId]) });
+      // For communication with the dragged task card, to denote that the
+      // task card has been moved and the original one can be removed
+      taskCard.dataset.isMoved = true;
+    }
   }
 
-  addTask() {
-    this.setState({ taskCardIds: this.state.taskCardIds.concat(null) });
+  // Remove dragged task card from original subboard once it has been moved
+  handleTaskCardDragEnd(event, taskCardIndex, taskCardHtmlId) {
+    const taskCard = document.getElementById(taskCardHtmlId);
+    // This attribute was used by the ondrop handler to denote that the task card
+    // has been moved
+    if (taskCard.dataset.isMoved) {
+      taskCard.removeAttribute("data-is-moved");
+      const taskCardIds = this.state.taskCardIds;
+      const taskCardIdsNew = taskCardIds.slice(0, taskCardIndex).concat(taskCardIds.slice(taskCardIndex + 1));
+      this.setState({ taskCardIds: taskCardIdsNew });
+    }
   }
 
-  getTaskCountDisplayElement() {
+  // Add task card to subboard
+  addTaskCard() {
+    const taskCardId = DbUtils.createTaskCard(this.state.db, USER_ID);
+    this.setState({ taskCardIds: this.state.taskCardIds.concat(taskCardId) });
+  }
+
+  // Create element that displays task count on subboard
+  createTaskCountDisplayElement() {
     const numTasks = this.state.taskCardIds.length;
     const taskCountText = `${numTasks} ${numTasks === 1 ? "task" : "tasks"}`;
     return (
@@ -88,20 +133,36 @@ class Subboard extends React.Component {
   }
 
   render() {
-    const taskCards = this.state.taskCardIds.map(function (item, index) {
-      return <TaskCard key={index} />
-    })
-    const taskCountDisplayElement = this.getTaskCountDisplayElement();
+    // Create task cards' JSX
+    const taskCards = this.state.taskCardIds.map(function (taskCardId, taskCardIndex) {
+      const taskCardHtmlId = this.props.subboardName + ":" + taskCardId + ":" + taskCardIndex;
+      // Note: React will re-render elements created using map function when 
+      // key changes, so set key to task card's html id, which will change
+      // when the position of a task card shifts
+      return (
+        <TaskCard
+          key={taskCardHtmlId}
+          taskCardIndex={taskCardIndex}
+          taskCardId={taskCardId}
+          taskCardHtmlId={taskCardHtmlId}
+          onDragEnd={((event, taskCardIndex, taskCardHtmlId) => this.handleTaskCardDragEnd(event, taskCardIndex, taskCardHtmlId)).bind(this)}
+          db={this.state.db}
+        />
+      )
+    }, this)
+
+    // Create task count JSX
+    const taskCountDisplayElement = this.createTaskCountDisplayElement();
 
     return (
       <section className="subboard"
-        onDragOver={(event) => this.allowElementDrop(event)}
-        onDrop={(event) => this.elementDrop(event)}
+        onDragOver={(event) => this.handleTaskCardDragOver(event)}
+        onDrop={(event) => this.handleTaskCardDrop(event)}
       >
         <header className="subboard-header">{this.props.subboardName}</header>
         { taskCountDisplayElement}
         { taskCards}
-        <button className="add-task-btn" onClick={() => this.addTask()}>
+        <button className="add-task-card-btn" onClick={() => this.addTaskCard()}>
           (+) ADD TASK
         </button>
       </section >
@@ -116,6 +177,9 @@ class Subboard extends React.Component {
 class Board extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      db: DB
+    }
   }
 
   render() {
@@ -125,9 +189,9 @@ class Board extends React.Component {
           React Assignment - Task Board
         </header>
         <div className="subboards">
-          <Subboard subboardName="Pending" />
-          <Subboard subboardName="In Progress" />
-          <Subboard subboardName="Completed" />
+          <Subboard subboardName="Pending" db={this.state.db} />
+          <Subboard subboardName="In Progress" db={this.state.db} />
+          <Subboard subboardName="Completed" db={this.state.db} />
         </div>
       </section>
     )
---
+++ src/utils/db-utils.js
@@ -0,0 +1,43 @@
+/**
+ * Create unique task card ID
+ * For this exercise, assume that max 1 task card 
+ * created per user per millisecond
+ * @param {string} userId
+ * @returns task card ID
+ */
+export function createTaskCardId(userId) {
+    return "USER" + userId + "TIME" + Date.now();
+}
+
+/**
+ * Initialize task card and save it to DB
+ * @param {Object} db 
+ * @param {string} userId 
+ * @returns ID of task card that was created
+ */
+export function createTaskCard(db, userId) {
+    const taskCardId = createTaskCardId(userId);
+    db[taskCardId] = { "header": "", "content": "" };
+    return taskCardId
+}
+
+/**
+ * Retrieve details of task card
+ * @param {Object} db 
+ * @param {string} taskCardId 
+ * @returns task card details
+ */
+export function retrieveTaskCardDetails(db, taskCardId) {
+    return db[taskCardId];
+}
+
+/**
+ * Save changes made to task card
+ * @param {Object} db 
+ * @param {string} cardId 
+ * @param {string} newText 
+ * @param {enum} cardPortion 
+ */
+export function saveTaskCardChanges(db, cardId, newText, cardPortion) {
+    db[cardId][cardPortion] = newText;
+}
